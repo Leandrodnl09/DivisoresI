1957e2dac91bbb9c6bc76c3b0469b187643ce695
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2022-08-03 19:09:31 -0300

    DivisoresI

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Exercício do site URI "Divisores I"
+// Esse programa o usuario irá digitar um número inteiro, e em seguida o programa vai mostrar na tela os numeros divisores de 1 até o numero digitado "N".
+
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i ++)
+{
+    if (N % i == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
